d4a4e6a0a0f2ac835c76f70323fadd380b4c7e2f
y.matsumoto <yunosukematsumoto.hal@gmail.com>
2018-08-09 23:40:41 +0900

    アニメーションデータを保持するクラスを定義

---
+++ Src/Animation3D.h
@@ -0,0 +1,18 @@
+/*=================================================
+//								FILE :	Animation3D.h
+//		ファイル説明 :
+//		アニメーションデータを保持するクラス
+//
+//									松本 雄之介
+=================================================*/
+#pragma once
+
+class cAnimation3D
+{
+public:
+	cAnimation3D() = default;
+	~cAnimation3D() = default;
+
+private:
+
+};
