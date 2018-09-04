5bdca2661e5971b8a0302e8da42aa6c1fe5c4f05
y.matsumoto <yunosukematsumoto.hal@gmail.com>
2018-09-04 13:13:23 +0900

    キー入力クラス追加

--- Src/Game/Pacman.h
+++ Src/Game/Pacman.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "../DXMath.h"
 
 class cPacman
 {
---
+++ Src/Input.cpp
@@ -0,0 +1,464 @@
+/*=========================================================//
+//		概要	:	入力処理を記述する
+//					作成者　松本 雄之介 (AT12A606)
+//						作成日	2016/11/2
+//					  更新日
+//
+//=========================================================*/
+#include "Input.h"
+#include "MainWindow.h"
+
+#define DIRECTINPUT_VERSION 0x0800
+//#define DEADZONE XINPUT_GAMEPAD_RIGHT_THUMB_DEADZONE
+#define DEADZONE 12000
+
+//他のファイルから隠ぺい
+namespace {
+	const int NUM_KEY_MAX = 256;
+	LPDIRECTINPUT8			pDInput = NULL;						// IDirectInput8インターフェースへのポインタ
+	LPDIRECTINPUTDEVICE8	pDIDevKeyboard = NULL;				// IDirectInputDevice8インターフェースへのポインタ(キーボード)
+	LPDIRECTINPUTDEVICE8	pDIDevMouse = NULL;					// IDirectInputDevice8インターフェースへのポインタ(マウス)
+	BYTE						keyState[NUM_KEY_MAX];				// キーボードの状態を受け取るワーク
+	BYTE						keyStateTrigger[NUM_KEY_MAX];		// キーボードの状態を受け取るワーク
+	BYTE						keyStateRepeat[NUM_KEY_MAX];		// キーボードの状態を受け取るワーク
+	BYTE						keyStateRelease[NUM_KEY_MAX];		// キーボードの状態を受け取るワーク
+	int							keyStateRepeatCnt[NUM_KEY_MAX];	// キーボードのリピートカウンタ
+	DIMOUSESTATE2			mouseState;							// マウスの状態を受け取るワーク
+	DIMOUSESTATE2			mouseStateTrigger;					// マウスの状態を受け取るワーク
+	POINT						MousePoint;							// マウスの現在座標
+
+	struct XInputLuaGlueStruct {
+		std::string vVarName;		//Luaに登録する変数名
+		unsigned int vNumber;		//登録する実数値
+	};
+}
+using namespace Input;
+
+/// <summary>
+/// キーボード情報を初期化
+/// </summary>
+cKeyboard::cKeyboard() {
+	HRESULT hr;
+	if (!pDInput)
+	{
+		// DirectInputオブジェクトの作成
+		hr = DirectInput8Create(cMainWindow::GetHInstance(), DIRECTINPUT_VERSION,
+			IID_IDirectInput8, (void**)&pDInput, NULL);
+	}
+	InitKeyboard(cMainWindow::GetHInstance(), cMainWindow::GetHWND());
+}
+
+/// <summary>
+/// キーボード情報を初期化し、システムから利用できるようにする
+/// </summary>
+/// <param name="hInst"></param>
+/// <param name="hWnd"></param>
+/// <returns></returns>
+HRESULT cKeyboard::InitKeyboard(HINSTANCE hInst, HWND hWnd) {
+	HRESULT hr = 0;
+
+	// デバイスオブジェクトを作成
+	hr = pDInput->CreateDevice(GUID_SysKeyboard, &pDIDevKeyboard, NULL);
+	if (FAILED(hr) || pDIDevKeyboard == NULL)
+	{
+		MessageBox(hWnd, "キーボードがねぇ！", "警告！", MB_ICONWARNING);
+		return hr;
+	}
+
+	// データフォーマットを設定
+	hr = pDIDevKeyboard->SetDataFormat(&c_dfDIKeyboard);
+	if (FAILED(hr))
+	{
+		MessageBox(hWnd, "キーボードのデータフォーマットを設定できませんでした。", "警告！", MB_ICONWARNING);
+		return hr;
+	}
+
+	// 協調モードを設定（フォアグラウンド＆非排他モード）
+	hr = pDIDevKeyboard->SetCooperativeLevel(hWnd, (DISCL_FOREGROUND | DISCL_NONEXCLUSIVE));
+	if (FAILED(hr))
+	{
+		MessageBox(hWnd, "キーボードの協調モードを設定できませんでした。", "警告！", MB_ICONWARNING);
+		return hr;
+	}
+
+	// キーボードへのアクセス権を獲得(入力制御開始)
+	pDIDevKeyboard->Acquire();
+
+	return S_OK;
+}
+
+/// <summary>
+/// キーの更新を行う。ループの最初に行うといい
+/// </summary>
+/// <returns></returns>
+HRESULT cKeyboard::UpdateKeyboard(void) {
+	HRESULT hr;
+	BYTE keyStateOld[256];
+
+	// 前回のデータを保存
+	memcpy(keyStateOld, keyState, NUM_KEY_MAX);
+
+	// デバイスからデータを取得
+	hr = pDIDevKeyboard->GetDeviceState(sizeof(keyState), keyState);
+	if (SUCCEEDED(hr))
+	{
+		for (int cnt = 0; cnt < NUM_KEY_MAX; cnt++)
+		{
+			// トリガーとリリースを取得
+			keyStateTrigger[cnt] = (keyState[cnt] ^ keyStateOld[cnt]) & keyState[cnt];
+			keyStateRelease[cnt] = (keyState[cnt] ^ keyStateOld[cnt]) & keyStateOld[cnt];
+
+			// キーが押されているならリピートの判定処理
+			if (keyState[cnt])
+			{
+				if (keyStateRepeatCnt[cnt] < 20)
+				{
+					keyStateRepeatCnt[cnt]++;
+					// 「初回入力」もしくは「ボタンを押してから20フレーム経過」
+					// した場合、リピート用配列のデータを有効にする
+					// 上記条件以外はリピート用配列のデータを無効にする
+					if (keyStateRepeatCnt[cnt] == 1 || keyStateRepeatCnt[cnt] >= 20) {
+						keyStateRepeat[cnt] = 0x80;
+					}
+					else {
+						keyStateRepeat[cnt] = 0;
+					}
+				}
+			}
+			else
+			{
+				keyStateRepeatCnt[cnt] = 0;
+				keyStateRepeat[cnt] = 0;
+			}
+		}
+	}
+	else
+	{
+		// キーボードへのアクセス権を取得
+		hr = pDIDevKeyboard->Acquire();
+	}
+
+	return hr;
+}
+
+/// <summary>
+/// 解放処理
+/// </summary>
+void cKeyboard::UninitKeyboard(void) {
+	if (pDIDevKeyboard)
+	{
+		pDIDevKeyboard->Unacquire();
+
+		pDIDevKeyboard->Release();
+		pDIDevKeyboard = nullptr;
+	}
+}
+
+/// <summary>
+/// 解放処理
+/// </summary>
+cKeyboard::~cKeyboard() {
+	// キーボードの終了処理
+	UninitKeyboard();
+
+	if (pDInput) {
+		pDInput->Release();
+		pDInput = nullptr;
+	}
+}
+
+/// <summary>
+/// 特定のキーが押されているか判定する
+/// </summary>
+/// <param name="key"></param>
+/// <returns></returns>
+bool cKeyboard::Press(int key) {
+	return ((keyState[key] & 0x80) != 0);
+}
+
+/// <summary>
+/// 特定のキーが押された瞬間か判定する
+/// </summary>
+/// <param name="key"></param>
+/// <returns></returns>
+bool cKeyboard::Trigger(int key) {
+	return ((keyStateTrigger[key] & 0x80) != 0);
+}
+
+/// <summary>
+/// 特定のキーが離された瞬間か判定する
+/// </summary>
+/// <param name="key"></param>
+/// <returns></returns>
+bool Input::cKeyboard::Release(int key)
+{
+	return (keyStateRelease[key]);
+}
+
+/// <summary>
+/// 特定のゲームパッドボタンが押されたか判定する
+/// </summary>
+/// <param name="key"></param>
+/// <param name="no"></param>
+/// <returns></returns>
+bool XInput::Press(const int key, const DWORD no) const
+{
+	if (conect[no]) {
+		if (m_State[no].Gamepad.wButtons & key) {
+			return true;
+		}
+	}
+	else {
+		if (GetKey->Press(GetGamePad->ConvertKeyboardToGamePad(key)))
+			return true;
+	}
+	return false;
+}
+
+/// <summary>
+/// 特定のゲームパッドボタンが押された瞬間か判定する
+/// </summary>
+/// <param name="key"></param>
+/// <param name="no"></param>
+/// <returns></returns>
+bool Input::XInput::Trigger(const int key, const DWORD no) const
+{
+	if (conect[no]) {
+		//現在のキーと前のキーとの差分処理で判定を行う
+		bool nowKey = m_State[no].Gamepad.wButtons & key;
+		bool oldKey = m_OldState[no].Gamepad.wButtons & key;
+		//トリガー判定処理
+		if ((nowKey ^ oldKey) & nowKey)
+			return true;
+	}
+	else {
+		if (GetKey->Trigger(GetGamePad->ConvertKeyboardToGamePad(key)))
+			return true;
+	}
+	return false;
+}
+
+/// <summary>
+/// 特定のゲームパッドボタンが離された瞬間か判定する
+/// </summary>
+/// <param name="key"></param>
+/// <param name="no"></param>
+/// <returns></returns>
+bool Input::XInput::Release(const int key, const DWORD no) const
+{
+	if (conect[no]) {
+		//現在のキーと前のキーとの差分処理で判定を行う
+		bool nowKey = m_State[no].Gamepad.wButtons & key;
+		bool oldKey = m_OldState[no].Gamepad.wButtons & key;
+		//リリース判定処理
+		if ((nowKey ^ oldKey) & oldKey)
+			return true;
+	}
+	else {
+		if (GetKey->Release(GetGamePad->ConvertKeyboardToGamePad(key)))
+			return true;
+	}
+	return false;
+}
+
+/// <summary>
+/// レフトトリガーの入力情報を取得する
+/// </summary>
+/// <param name="no"></param>
+/// <returns>押し込み具合(0.0 ~ 1.0)</returns>
+float Input::XInput::LeftTrigger(const DWORD no) const
+{
+	if (conect[no]) {
+		//数値をチェックし、0.0~1.0の間で返却する
+		float buf = (float)m_State[no].Gamepad.bLeftTrigger / 255.0f;
+		return buf;
+	}
+	else {
+		if (GetKey->Press(DIK_Q)) {
+			return 1.0f;
+		}
+	}
+	return 0.0f;
+}
+
+/// <summary>
+/// ライトトリガーの入力情報を取得する
+/// </summary>
+/// <param name="no"></param>
+/// <returns>押し込み具合(0.0 ~ 1.0)</returns>
+float Input::XInput::RightTrigger(const DWORD no) const
+{
+	if (conect[no]) {
+		//数値をチェックし、0.0~1.0の間で返却する
+		float buf = (float)m_State[no].Gamepad.bRightTrigger / 255.0f;
+		return buf;
+	}
+	else {
+		if (GetKey->Press(DIK_E)) {
+			return 1.0f;
+		}
+	}
+	return 0.0f;
+}
+
+/// <summary>
+/// 左スティックの入力情報を取得する
+/// </summary>
+/// <param name="no"></param>
+/// <returns>倒され具合(0.0 ~ 1.0)</returns>
+DirectX::XMFLOAT2 Input::XInput::LeftStick(const DWORD no) const
+{
+	DirectX::XMFLOAT2 judge{ 0 , 0 };
+	if (conect[no]) {
+		judge.x = (float)m_State[no].Gamepad.sThumbLX / 32767;
+		judge.y = (float)m_State[no].Gamepad.sThumbLY / 32767;
+		return judge;
+	}
+	else {
+		//コントローラがつながっていない
+		if (GetKey->Press(DIK_A)) {
+			judge.x -= 1.0f;
+		}
+		if (GetKey->Press(DIK_D)) {
+			judge.x += 1.0f;
+		}
+		if (GetKey->Press(DIK_W)) {
+			judge.y += 1.0f;
+		}
+		if (GetKey->Press(DIK_S)) {
+			judge.y -= 1.0f;
+		}
+	}
+
+	return judge;
+}
+
+/// <summary>
+/// 右スティックの入力情報を取得する
+/// </summary>
+/// <param name="no"></param>
+/// <returns>倒され具合(0.0 ~ 1.0)</returns>
+DirectX::XMFLOAT2 Input::XInput::RightStick(const DWORD no) const
+{
+	DirectX::XMFLOAT2 judge{ 0 , 0 };
+	if (conect[no]) {
+		judge.x = (float)m_State[no].Gamepad.sThumbRX / 32767;
+		judge.y = (float)m_State[no].Gamepad.sThumbRY / 32767;
+		return judge;
+	}
+	else {
+		//コントローラがつながっていない
+		if (GetKey->Press(DIK_LEFT)) {
+			judge.x -= 1.0f;
+		}
+		if (GetKey->Press(DIK_RIGHT)) {
+			judge.x += 1.0f;
+		}
+		if (GetKey->Press(DIK_UP)) {
+			judge.y += 1.0f;
+		}
+		if (GetKey->Press(DIK_DOWN)) {
+			judge.y -= 1.0f;
+		}
+	}
+	return judge;
+}
+
+/// <summary>
+/// キー情報の更新を行う。ループの最初に一度行うと良い。
+/// </summary>
+void XInput::UpdateGamePad(void)
+{
+	//ステートとストロークを取得する
+	for (DWORD i = 0; i < XUSER_MAX_COUNT; i++) {
+		m_OldState[i] = m_State[i];		//古いキー情報を格納する
+		DWORD hr = XInputGetState(i, &m_State[i]);	//キー取得
+
+		//ゲームパッドが接続されていない
+		if (hr == ERROR_DEVICE_NOT_CONNECTED) {
+			conect[i] = false;		//接続フラグオフ
+			continue;
+		}
+
+		//接続されている
+		conect[i] = true;
+
+		//デッドゾーンの設定。デッドゾーン内なら数値を0に変更しておく
+		if ((m_State[i].Gamepad.sThumbLX < DEADZONE &&
+			m_State[i].Gamepad.sThumbLX > -DEADZONE)) {
+			m_State[i].Gamepad.sThumbLX = 0;
+		}
+		if (m_State[i].Gamepad.sThumbLY < DEADZONE &&
+			m_State[i].Gamepad.sThumbLY > -DEADZONE) {
+
+			m_State[i].Gamepad.sThumbLY = 0;
+		}
+
+		if ((m_State[i].Gamepad.sThumbRX < DEADZONE &&
+			m_State[i].Gamepad.sThumbRX > -DEADZONE)) {
+			m_State[i].Gamepad.sThumbRX = 0;
+		}
+		if (m_State[i].Gamepad.sThumbRY < DEADZONE &&
+			m_State[i].Gamepad.sThumbRY > -DEADZONE) {
+
+			m_State[i].Gamepad.sThumbRY = 0;
+		}
+	}
+}
+
+/// <summary>
+/// ゲームパッドが存在しない場合にキー情報をキーボードに割り当てる
+/// </summary>
+/// <param name="key"></param>
+/// <returns></returns>
+unsigned Input::XInput::ConvertKeyboardToGamePad(unsigned key)
+{
+	unsigned type = 0;
+	switch (key)
+	{
+	case XINPUT_GAMEPAD_DPAD_UP:
+		type = DIK_W;
+		break;
+	case XINPUT_GAMEPAD_DPAD_DOWN:
+		type = DIK_S;
+		break;
+	case XINPUT_GAMEPAD_DPAD_LEFT:
+		type = DIK_A;
+		break;
+	case XINPUT_GAMEPAD_DPAD_RIGHT:
+		type = DIK_D;
+		break;
+	case XINPUT_GAMEPAD_START:
+		type = DIK_C;
+		break;
+	case XINPUT_GAMEPAD_BACK:
+		type = DIK_Z;
+		break;
+	case XINPUT_GAMEPAD_LEFT_THUMB:
+		type = DIK_R;
+		break;
+	case XINPUT_GAMEPAD_RIGHT_THUMB:
+		type = DIK_T;
+		break;
+	case XINPUT_GAMEPAD_LEFT_SHOULDER:
+		type = DIK_Q;
+		break;
+	case XINPUT_GAMEPAD_RIGHT_SHOULDER:
+		type = DIK_E;
+		break;
+	case XINPUT_GAMEPAD_A:
+		type = DIK_DOWN;
+		break;
+	case XINPUT_GAMEPAD_B:
+		type = DIK_RIGHT;
+		break;
+	case XINPUT_GAMEPAD_X:
+		type = DIK_LEFT;
+		break;
+	case XINPUT_GAMEPAD_Y:
+		type = DIK_5;
+		break;
+	}
+
+	return type;
+}
